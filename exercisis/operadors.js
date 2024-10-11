c5a86a95ba2be0a36d82356ca5647d2a1ea34619
remeisarrias <1706840@uab.cat>
2024-10-11 16:51:00 +0200

    primer

---
+++ exercisis/operadors.js
@@ -0,0 +1,11 @@
+//4.2.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+let a = 10;
+let b = 20;
+let c = 5;
+a = a + 3;
+b = b + 4 - a;
+c = a + b + c;
+a = a + c;
+b = 4;
+c = c + 3 - b + 2;
+console.log(a,b,c);
